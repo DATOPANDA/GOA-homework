d11e523d5dd9a8f87eb95a1d1cd1f1ac421aeecc
DATOPANDA <dtatrishvili2@gmail.com>
2025-10-01 17:24:34 +0300

    day 1 homework

---
+++ classwork.homework/homework/level 1 homework/homework level 1.py
@@ -0,0 +1,26 @@
+print ("----------------------------------------------------------------------------------------------------------------------------------")
+print ("Name")
+myname = "Dato Tatrishvili"
+
+print (myname)
+
+num1 = 5
+
+num2 = 20
+print ("----------------------------------------------------------------------------------------------------------------------------------")
+print ("resoult")
+
+resoult = num1 + num2
+
+print (resoult)
+print ("----------------------------------------------------------------------------------------------------------------------------------")
+print ("difference")
+
+difference = num2 - num1
+
+print (difference)
+print ("----------------------------------------------------------------------------------------------------------------------------------")
+print ("what is print: print is phyton function that sending printed word, sentences or numbers from Code redactor window to the screen and to the terminal")
+print ("----------------------------------------------------------------------------------------------------------------------------------")
+print ("what is variable: variable is phyton function that save many other words in one word like a box when u contain data or value its can be text, number, boolean value, list, object, and other data types that are then used in the program in this way 1 word like 'company' can be meaned 'apple,google,microsoft,nvidia' and more ")
+print ("----------------------------------------------------------------------------------------------------------------------------------")
